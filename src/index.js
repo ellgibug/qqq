223a832d8129bd398a2986f2ffb7226fa463f23b
Ольга Фоменко <olgafomenko@MacBook-Pro-Olga.local>
2020-01-19 17:21:32 +0300

    floors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import * as $ from 'jquery'
 import Post from '@models/Post';
 import json from './assets/json'
 import image from './assets/image.jpg'
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 // import xml from './assets/data.xml'
 // import csv from './assets/data.csv'
-import './styles/styles.css'
-import './styles/sass.sass'
-import './styles/badscript.css'
+// import './styles/styles.css'
+// import './styles/sass.sass'
+import './styles/floors.scss'
+// import './styles/badscript.css'
+ import './styles/caviardreams.css'
 
 // тоже самое с less, только надо скачать less-loader
 // scss можно использовать как sass
